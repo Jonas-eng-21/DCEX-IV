cd87901736c5684c42bc35bfa075e7d12c2f25a3
Jonas-eng-21 <jonas.soares@comeialabs.com.br>
2023-03-14 22:41:44 -0300

    Configuração inicial do projeto

---
+++ my-app/src/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const AppContainer = styled.div`
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f9f9f9;
+`;
