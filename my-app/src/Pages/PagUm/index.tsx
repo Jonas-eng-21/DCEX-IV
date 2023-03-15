a7630dad01e9b82a34f4a777ff01b7110b36264c
Jonas-eng-21 <jonas.soares@comeialabs.com.br>
2023-03-15 09:06:52 -0300

    Mudanças iniciais no porjeto

---
+++ my-app/src/Pages/PagUm/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "./logo.svg";
+import { AppContainer } from "./style";
+
+export default function PagUm() {
+  return (
+    <AppContainer>
+      <h1>Conhecendo a UPE</h1>
+    </AppContainer>
+  );
+}
---
+++ my-app/src/Pages/home/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "./logo.svg";
+import { AppContainer } from "./style";
+
+export default function Home() {
+  return (
+    <AppContainer>
+      <h1>Hello, world!</h1>
+    </AppContainer>
+  );
+}
